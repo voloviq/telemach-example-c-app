abb5525ff258cb25f7a83d8fbcb39420ea7024ae
Michal Wolowik <michwolo@gmail.com>
2025-03-26 00:07:23 +0100

    Added another example

---
+++ show-current-time/showtime.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+ * Simple application which print message "Hello world" on the console
+ * MW Sojfan 2025
+ * Rev v1.0
+ *****************************************************************************/
+
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    // Get the current time
+    time_t now = time(NULL);
+    struct tm *local_time = localtime(&now);
+
+    printf("Hello World! Current time: %02d:%02d:%02d\n",
+        local_time->tm_hour, local_time->tm_min, local_time->tm_sec);
+
+    return 0; 
+}
